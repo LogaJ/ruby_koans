51aec6f435116650785ef5904387c60060c7e73b
Loga Jegede <loga.jegede@net-a-porter.com>
2012-03-01 17:34:08 +0000

    completed triangle one koan.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if a == b and b == c
+  if a == b || b == c || c == a
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
